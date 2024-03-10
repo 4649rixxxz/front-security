63eb4511ce0d60b1dec830279f633903ed5885d9
Ishikawa Ryota <lhasryota@gmail.com>
2024-03-10 09:49:32 +0900

    [update]クリックジャッキング対策

--- app/index.js
+++ app/index.js
@@ -3,7 +3,13 @@ const api = require('./routes/api');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public', {
+  setHeaders: (res, path, stat) => {
+    // 同一ドメインによるiframeでの表示を許可
+    res.header('X-Frame-Options', 'SAMEORIGIN');
+  }
+}));
+
 app.use('/api', api);
 
 app.get('/', (req, res) => {
